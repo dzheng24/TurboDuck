583fe768b3eabda922137c42a2f4d47faf7f3735
David Zheng <zhengdav@msu.edu>
2020-06-22 14:28:58 -0700

    re-added files from davidMonday branch

--- TurboDuckRace/src/com/huskies/turboduck/Duck.java
+++ TurboDuckRace/src/com/huskies/turboduck/Duck.java
@@ -1,19 +1,43 @@
 package com.huskies.turboduck;
 
+import java.text.DecimalFormat;
+
 public class Duck {
 
-    String name;
-    Color color;
-    Point point;
+    // FIELDS
+    private String name;
+    private Color color;
+    private Point distancePoint;
 
+    // CTORS
     public Duck() {
-        this("Some duck I found in my memory", Color.YELLOW);
+        this("default duck", Color.YELLOW);
     }
 
     public Duck(String name, Color color) {
         this.name = name;
         this.color = color;
-        point = new Point();
+        this.distancePoint = new Point();
+    }
+
+    // BUSINESS METHODS
+    public void move() {
+        // DONE
+        double randomIncrement = Math.random();
+        DecimalFormat df = new DecimalFormat("#.##");
+        randomIncrement = Double.valueOf(df.format(randomIncrement));
+        this.distancePoint.changeRacerPosition(randomIncrement);
+        System.out.println("Duck has moved " + randomIncrement + " spots, and is now at " + this.getDistanceTraveled());
+
+    }
+
+    // GETTERS/SETTERS
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
     }
 
     public String getName() {
@@ -24,11 +48,8 @@ public class Duck {
         return color;
     }
 
-    public Point getPoint() {
-        return point;
-    }
-
-    public void move() {
-        // TODO
+    public double getDistanceTraveled() {
+        return distancePoint.getxPosition();
     }
 }
+
--- TurboDuckRace/test/com/huskies/turboduck/DuckTest.java
+++ TurboDuckRace/test/com/huskies/turboduck/DuckTest.java
@@ -6,4 +6,35 @@ import static org.junit.Assert.*;
 
 public class DuckTest {
 
+    @Test
+    public void testSingleDuck() {
+        Duck testDuck = new Duck();
+
+        assertEquals("default duck", testDuck.getName());
+        assertEquals(Color.YELLOW, testDuck.getColor());
+    }
+
+    @Test
+    public void testSingleDuckWithCustomParams() {
+        Duck testDuck = new Duck("David", Color.BLUE);
+
+        assertEquals("David", testDuck.getName());
+        assertEquals(Color.BLUE, testDuck.getColor());
+        assertEquals(0.0, testDuck.getDistanceTraveled(), .001);
+    }
+
+    @Test
+    public void testingDistanceTraveled() {
+        Duck testDuck = new Duck("Jay", Color.GREEN);
+
+        assertEquals(0.0, testDuck.getDistanceTraveled(), .001);
+        testDuck.move();
+        // tell the duck to move again
+        testDuck.move();
+        // and again...
+        testDuck.move();
+        System.out.println();
+        System.out.println(testDuck.getDistanceTraveled());
+
+    }
 }
